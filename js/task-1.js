b37d84e076458452d0596fdcf0e7b8f7aa00bcb4
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-12-18 23:57:51 +0200

    added

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,14 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits);
 
 
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
+  const message = customerCredits >= totalPrice
+    ? `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    : 'Insufficient funds!';
+  return message;
+}
 
+// Перевірка роботи функції
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
